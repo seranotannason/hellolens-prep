50cf9ea699229164cb955f2a7e2ac3dea37b4eb0
Steven Shan <steven.shan99@gmail.com>
2018-09-15 15:51:04 -0400

    working on streaming audio

---
+++ Assets/Scripts/AudioTranslator.cs
@@ -0,0 +1,132 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Xml.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+public delegate void ReceiveHandler(string val);
+
+// to get supported languages make a get request to
+// Accept: application/json
+// https://dev.microsofttranslator.com/languages?api-version=1.0&scope=speech
+
+// open a connection to...
+// wss://dev.microsofttranslator.com/speech/translate
+
+// Example:
+// GET wss://dev.microsofttranslator.com/speech/translate?from=en-US&to=it-IT&features=texttospeech&voice=it-IT-Elsa&api-version=1.0
+// Ocp-Apim-Subscription-Key: {subscription key}
+// X-ClientTraceId: {GUID}
+
+// Once the connection is established, the client begins streaming audio to the service.The client sends audio in chunks.
+// Each chunk is transmitted using a Websocket message of type Binary.
+// Audio input is in the Waveform Audio File Format(WAVE, or more commonly known as WAV due to its filename extension). 
+// The client application should stream single channel, signed 16bit PCM audio sampled at 16 kHz.
+// The first set of bytes streamed by the client will include the WAV header.
+// A 44-byte header for a single channel signed 16 bit PCM stream sampled at 16 kHz is:
+public class AudioTranslator : MonoBehaviour
+{
+    public static AudioTranslator instance;
+        
+    // Public fields accessible in the Unity Editor
+    public string apiKey = "955f6747399a477e8f25c0fc52a2a1d6";
+
+    private string translationTokenEndpoint = "https://api.cognitive.microsoft.com/sts/v1.0/issueToken";
+
+    private ClientWebSocket client;
+    private bool ready = false;
+
+    public event ReceiveHandler Received;
+
+    async void Start()
+    {
+        /*
+        string from = "en-US";
+        string to = "it-IT";
+        string voice = "it-IT-Elsa";
+        var uri = $"wss://dev.microsofttranslator.com/speech/translate?from={from}&to={to}&features=partial,texttospeech&voice={voice}&api-version=1.0";
+        */
+        string from = "en-US";
+        string to = "it-IT";
+        string features = "texttospeech";
+        string voice = "it-IT-Elsa";
+        string api = "1.0";
+        string host = "wss://dev.microsofttranslator.com";
+        string path = "/speech/translate";
+        string uri = host + path +
+            "?from=" + from +
+            "&to=" + to +
+            "&api-version=" + api +
+            "&features=" + features +
+            "&voice=" + voice;
+
+        Debug.Log("starting web socket");
+        client = new ClientWebSocket();
+        client.Options.SetRequestHeader("Ocp-Apim-Subscription-Key", apiKey);
+
+        await client.ConnectAsync(new Uri(uri), CancellationToken.None);
+        Debug.Log("connected to socket");
+
+        var wavHeader = new byte[44] {82, 73, 70, 70, 0, 0, 0, 0, 87, 65, 86, 69, 0, 102, 109, 116, 0, 0, 0, 16, 0, 1, 0, 1, 0, 0, 62, 128, 0, 0, 125, 0, 0, 2, 0, 16, 100, 97, 116, 97, 0, 0, 0, 0};
+        var wavHeaderBuffer = new ArraySegment<byte>(wavHeader);
+        send(wavHeaderBuffer);
+        ready = true;
+
+        Task.WhenAll(receiveData()).Wait();
+    }
+
+    private async void send(ArraySegment<byte> buff)
+    {
+        await client.SendAsync(buff, WebSocketMessageType.Binary, true, CancellationToken.None);
+    }
+
+    private async Task receiveData()
+    {
+        var inbuf = new byte[102400];
+        var segment = new ArraySegment<byte>(inbuf);
+
+        Console.WriteLine("Awaiting response.");
+        while (client.State == WebSocketState.Open)
+        {
+            var result = await client.ReceiveAsync(segment, CancellationToken.None);
+            switch (result.MessageType)
+            {
+                case WebSocketMessageType.Close:
+                    Console.WriteLine("Received close message. Status: " + result.CloseStatus + ". Description: " + result.CloseStatusDescription);
+                    await client.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+                    break;
+                case WebSocketMessageType.Text:
+                    Console.WriteLine("Received text.");
+                    Console.WriteLine(Encoding.UTF8.GetString(inbuf).TrimEnd('\0'));
+                    break;
+                case WebSocketMessageType.Binary:
+                    Console.WriteLine("Received binary data: " + result.Count + " bytes.");
+                    break;
+            }
+        }
+    }
+        
+    public async Task WriteData(MemoryStream stream, int count)
+    {
+        if (!ready)
+        {
+            return;
+        }
+        ArraySegment<byte> buffer;
+        if (stream.TryGetBuffer(out buffer))
+        {
+            send(buffer);
+        }
+    }
+
+}
---
+++ Assets/Scripts/MicrophoneAudioGetter.cs
@@ -0,0 +1,162 @@
+﻿#define DONT_DOWNSAMPLE
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class MicrophoneAudioGetter : MonoBehaviour
+{
+    AudioClip _clip;
+    string _mic;
+
+    public int SampleRate = 16000;
+
+    float[] audioData;
+    int _totalRead = 0;
+
+    int _lastRead = 0;
+
+    // 100ms of data
+    public int ChunkSize = 1600;
+
+    MemoryStream dataStream = new MemoryStream();
+
+    ArraySegment<byte> _buffer = new ArraySegment<byte>();
+
+    /// <summary>
+    /// Convert floating point audio data in the range -1.0 to 1.0 to signed 16-bit audio data
+    /// </summary>
+    /// <param name="audioData"></param>
+    /// <param name="stream"></param>
+    /// <returns></returns>
+    int BufferConvertedData(float[] audioData, Stream stream)
+    {
+#if DONT_DOWNSAMPLE
+        // Can't just do a block copy here as we need to convert from float[-1.0f, 1.0f] to 16bit PCM
+        int i = 0;
+        while (i < audioData.Length)
+        {
+            stream.Write(BitConverter.GetBytes(Convert.ToInt16(audioData[i] * Int16.MaxValue)), 0, sizeof(Int16));
+            ++i;
+        }
+#else
+        // Downsample from 48Khz tp 16kHz
+        for (int i=0;i<audioData.Length/3;i+=3)
+        {
+            float avg = (audioData[i] + audioData[i + 1] + audioData[i + 2]) / 3.0f;
+            stream.Write(BitConverter.GetBytes(Convert.ToInt16(avg * Int16.MaxValue)), 0, sizeof(Int16));
+        }
+#endif
+
+        return audioData.Length;
+    }
+
+    public bool IsRecording()
+    {
+        return _clip != null;
+    }
+
+    public void StartRecording()
+    {
+        // From here we can start streaming data from the mic...
+        if (Microphone.devices.Length > 0)
+        {
+            _mic = Microphone.devices[0];
+
+            Debug.Log("Num Microphones " + Microphone.devices.Length);
+            foreach (var mic in Microphone.devices)
+            {
+                Debug.Log(mic);
+            }
+
+            int minFreq = 0;
+            int maxFreq = 0;
+
+            Microphone.GetDeviceCaps(_mic, out minFreq, out maxFreq);
+            Debug.Log("Microphone min freq = " + minFreq + " max freq = " + maxFreq);
+
+            //AudioSettings.outputSampleRate = 16000;
+
+            // if we have a mic start capturing and streaming audio...
+            _clip = Microphone.Start(_mic, true, 1, SampleRate);
+            int numChannels = _clip.channels;
+            Debug.Log("Microphone clip num channels = " + numChannels);
+            while (Microphone.GetPosition(_mic) < 0) { } // HACK from Riro
+            Debug.Log("Recording started...");
+        }
+        else
+        {
+            Debug.Log("No Microphone detected");
+        }
+    }
+
+    public void StopRecording()
+    {
+        _clip = null;
+    }
+
+    IEnumerator AutoStart()
+    {
+        yield return new WaitForSeconds(5);
+        Debug.Log("Recording started");
+        StartRecording();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (_clip == null)
+            return;
+
+        int currentPos = Microphone.GetPosition(_mic);
+
+        // float loop = _clip.length * SampleRate;
+        if (currentPos < _lastRead)
+        {
+            _lastRead = 0;
+        }
+        if ((currentPos - _lastRead) == 0)
+            return;
+
+        // Every time we are called we need to buffer the audio data - otherwise
+        // when the data loops it will be lost..
+        // So copy and convert data here into a MemoryStream and then when  we hit a 
+        // chunk boundary we can empty it and either send to the network or copy to 
+        // disk..
+        // The sample count is determined by the length of the float array so we can't 
+        // optimise this allocation
+        audioData = new float[currentPos - _lastRead];
+
+        //Debug.Log("audiodata length = " + audioData.Length + " last read = " + _lastRead);
+        if (_clip.GetData(audioData, _lastRead))
+        {
+            _totalRead += BufferConvertedData(audioData, dataStream);
+            _lastRead = currentPos;
+        }
+
+        // When we have collected enough data for a chunk, send it
+        // and reset the collection..
+        if (dataStream.Position >= ChunkSize)
+        {
+            dataStream.Flush();
+            if (!dataStream.TryGetBuffer(out _buffer))
+            {
+                Debug.Log("Couldn't read buffer");
+                return;
+            }
+
+            var consumer = AudioTranslator.instance;
+            consumer.WriteData(dataStream, (int)dataStream.Position);
+
+            dataStream.Seek(0, SeekOrigin.Begin);
+            dataStream.SetLength(0);
+        }
+    }
+}
